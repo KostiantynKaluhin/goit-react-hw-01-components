d73378f1c8c96b5f7ec1053258f8f254f47c4bf7
Kostianyn <odessavshok@gmail.com>
2021-12-21 01:14:04 +0200

    add Friends

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem';
+import s from './FriendList.module.css';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={s.friendList}>
+      {friends.map(({ id, name, avatar, isOnline }) => (
+        <FriendListItem
+          key={id}
+          name={name}
+          avatar={avatar}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  ).isRequired,
+};
+
+export default FriendList;
